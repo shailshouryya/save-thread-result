6860b90042402e59023618ed63ddac2ed39e10de
Shail Shouryya <shailshouryya@gmail.com>
2021-01-09 20:16:28 -0800

    Add package metadata to __init__.py

--- python/save_thread_result/__init__.py
+++ python/save_thread_result/__init__.py
@@ -1,1 +1,22 @@
 from .thread_with_result import ThreadWithResult
+
+'''
+version:              0.0.0
+author:               Shail-Shouryya
+email:                shailshouryya@gmail.com
+development_status:   4 - Beta
+intended_audience:    Developers
+license:              OSI Approved :: Apache License 2.0
+ideal_python_version: Python 3.6+
+source:               https://github.com/Shail-Shouryya/save-thread-result
+'''
+
+
+__version__              = '0.0.0'
+__author__               = 'Shail-Shouryya'
+__email__                = 'shailshouryya@gmail.com'
+__development_status__   = '4 - Beta'
+__intended_audience__    = 'Developers'
+__license__              = 'OSI Approved :: Apache License 2.0'
+__ideal_python_version__ = 'Python 3.6+'
+__source__               = 'https://github.com/Shail-Shouryya/save-thread-result/python'
