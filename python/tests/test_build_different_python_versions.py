65eca00a93ae752ab4f14a9636f8e41f2182ac62
slow-but-steady <slowbutsteady1234@gmail.com>
2023-12-12 20:14:40 -0800

    Use template to create Dockerfile for testing
    - effectively reverses the
      "Remove `Dockerfile_template`" commit
      - this works since the
        different python versions DID NOT
        have different build requirements
        and actually ended up being very similar

--- python/tests/test_build_different_python_versions.py
+++ python/tests/test_build_different_python_versions.py
@@ -12,34 +12,72 @@ for the Docker image to build correctly.
 
 import os
 
+from typing import (
+    Sequence,
+)
+
 def main(
 ) -> None:
     environments = [
-        'debian-bullseye-slim-python3_0-from_source',
-        'debian-bullseye-slim-python3_1-from_source',
-        'debian-bullseye-slim-python3_2-from_source',
-        'debian-bullseye-slim-python3_3-from_source',
-        'debian-bullseye-slim-python3_4-from_source',
-        'debian-bullseye-slim-python3_5-from_source',
-        'debian-bullseye-slim-python3_6-from_source',
-        'debian-bullseye-slim-python3_7-from_source',
-        'debian-bullseye-slim-python3_8-from_source',
-        'debian-bullseye-slim-python3_9-from_source',
-        'debian-bullseye-slim-python3_10-from_source',
-        'debian-bullseye-slim-python3_11-from_source',
-        'debian-bullseye-slim-python3_12-from_source',
+        'template-debian-bullseye-slim-python-MAJOR_MINOR-from_source-version_0',
+        'template-debian-bullseye-slim-python-MAJOR_MINOR-from_source-version_legacy_1',
     ]
     for environment in environments:
-        dockerfile_location = ['.', 'dockerfiles', environment]
-        build_docker_image(dockerfile_location)
+        dockerfile_template_location = ['.', 'dockerfiles', environment]
+        write_dockerfiles(dockerfile_template_location)
+    for directory, _, files in os.walk('.'):
+        files.sort() # go through files in sorted order by name
+        for file in files:
+            if file.startswith('from_template-'):
+                dockerfile_location = os.path.join(directory, file)
+                build_docker_image(dockerfile_location)
+
+
+def read_file(
+    file_location: Sequence[str]
+) -> str:
+    formatted_dockerfile_template_location = os.path.join(*file_location)
+    with open(file=formatted_dockerfile_template_location, mode='r', buffering=-1, encoding='utf-8', newline=None) as file:
+        return file.read()
+
+def write_dockerfiles(
+    dockerfile_template_location: Sequence[str],
+) -> None:
+    formatted_dockerfile_template_location = os.path.join(*dockerfile_template_location)
+    dockerfile_template                    = read_dockerfile_template(formatted_dockerfile_template_location)
+    dockerfile_template_for_versions       = dockerfile_template_location[::] # make a copy to avoid modifying original object
+    dockerfile_template_for_versions[-1]  += '-versions'                      #
+    formatted_dockerfile_template_for_versions = os.path.join(*dockerfile_template_for_versions)
+    with open(file=formatted_dockerfile_template_for_versions, mode='r', buffering=-1, encoding='utf-8', newline=None) as dockerfile_template_for_versions_file:
+        for build_version in dockerfile_template_for_versions_file:
+            write_dockerfile_from_template(build_version, dockerfile_template, formatted_dockerfile_template_location)
+
+def read_dockerfile_template(
+    formatted_dockerfile_template_location: str,
+) -> str:
+    with open(formatted_dockerfile_template_location, mode='r', buffering=-1, encoding='utf-8', newline=None) as file:
+        dockerfile_template = file.read()
+    return dockerfile_template
+
+def write_dockerfile_from_template(
+    build_version: str,
+    dockerfile_template: str,
+    formatted_dockerfile_template_location: str,
+) -> None:
+    build_version             = build_version.strip()
+    major_minor               = '.'.join(build_version.split('.')[:-1:]) # remove the patch version
+    formatted_major_minor     = major_minor.replace('.', '_')
+    dockerfile_write_location = formatted_dockerfile_template_location.replace('-MAJOR_MINOR', formatted_major_minor).replace('template', f'from_template-{formatted_major_minor}').split('-version_')[0]
+    with open(file=dockerfile_write_location, mode='w', buffering=-1, encoding='utf-8', newline=None) as file:
+        file.write(dockerfile_template.format(MAJOR_MINOR_PATCH=build_version, MAJOR_MINOR=major_minor))
 
 
 def build_docker_image(
     dockerfile_location: str,
 ) -> None:
-    formatted_dockerfile_location = os.path.join(*dockerfile_location)
-    formatted_docker_image_tag = dockerfile_location[-1]
-    os.system(f'docker build --tag save_thread_result-{formatted_docker_image_tag} --file {formatted_dockerfile_location} .')
+    docker_tag_base = dockerfile_location.split('from_template-')[-1]
+    # print(docker_tag_base, dockerfile_location)
+    os.system(f'docker build --tag save_thread_result-{docker_tag_base} --file {dockerfile_location} .')
 
 
 if __name__ == '__main__':
